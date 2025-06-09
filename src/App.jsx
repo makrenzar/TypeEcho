bd51477fc9dddecf4840042056e41b2422fa8245
Mark Lawrence Zaragoza <markzaragoza301@gmail.com>
2025-06-09 14:10:54 +0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
         onChange={(e) => setText(e.target.value)}
         className="text-lg border border-gray-300 rounded-lg px-4 py-2 w-full max-w-[500px] mx-auto focus:outline-none focus:ring-2 focus:ring-blue-400"
       />
-      <p className="text-xs bottom-0 mb-1">Mark Lawrence Zaragoza</p>
+
     </div>
 
       
